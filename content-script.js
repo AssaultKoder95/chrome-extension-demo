70844d070aab32dc5bf1e8b878d63d9d1332f301
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2022-03-09 20:18:06 +0530

    feat: added toast notification css and js

--- content-script.js
+++ content-script.js
@@ -1,18 +1,35 @@
 setTimeout(() => {
-  function testAlert() {
-    alert("Calling Alert!");
+  let timerInstance;
+  let toastInstance;
+
+  function onClickHandler(btnText) {
+    toastInstance = document.getElementById("toast");
+    const messageBox = document.getElementById("toast-message");
+    messageBox.innerHTML = btnText + " button clicked successfully!";
+    showToast();
   }
 
+  function showToast() {
+    clearTimeout(timerInstance);
+    toastInstance.style.transform = "translateX(0px)";
+    timerInstance = setTimeout(() => {
+      toastInstance.style.transform = "translateX(400px)";
+    }, 4000);
+  }
+
+  function closeToast() {
+    toastInstance.style.transform = "translateX(500px)";
+  }
+
+  // Inject button list display html code in page
   const mainElement = document.getElementsByClassName("app_container");
 
-  const containerDiv = document.createElement("div");
-  containerDiv.id = "digital-brain-chrome-extension-1-0-0-main-box";
+  const container = document.createElement("div");
+  container.id = "digital-brain-chrome-extension-1-0-0-main-box";
 
-  const parentDiv = document.createElement("div");
-  parentDiv.id = "digital-brain-chrome-extension-1-0-0-content-box";
-  parentDiv.classList.add(
-    "digital-brain-chrome-extension-1-0-0-dynamic-item-box"
-  );
+  const parent = document.createElement("div");
+  parent.id = "digital-brain-chrome-extension-1-0-0-content-box";
+  parent.classList.add("digital-brain-chrome-extension-1-0-0-dynamic-item-box");
 
   const btnList = [
     "Refund Stripe Transaction",
@@ -25,11 +42,44 @@ setTimeout(() => {
     btn.innerHTML = btnText;
     btn.classList.add("digital-brain-chrome-extension-1-0-0-btn");
     btn.name = "notificationBtn";
-    btn.onclick = testAlert;
-    parentDiv.appendChild(btn);
+    btn.onclick = () => onClickHandler(btnText);
+    parent.appendChild(btn);
   });
 
-  containerDiv.appendChild(parentDiv);
+  container.appendChild(parent);
 
-  mainElement[0].parentElement.prepend(containerDiv);
-}, 3000);
+  mainElement[0].parentElement.prepend(container);
+
+  // Inject toast html code in page
+  const wrapper = document.createElement("div");
+  wrapper.id = "digital-brain-chrome-extension-1-0-0-toast";
+  wrapper.classList.add("wrapper");
+
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.id = "toast";
+
+  const toastBody = document.createElement("div");
+  toastBody.classList.add("toast-body");
+
+  const toastNotificationHeader = document.createElement("p");
+  toastNotificationHeader.innerHTML = "Success";
+
+  const toastMessageBody = document.createElement("p");
+  toastMessageBody.id = "toast-message";
+
+  toastBody.appendChild(toastNotificationHeader);
+  toastBody.appendChild(toastMessageBody);
+
+  let closeBtn = document.createElement("button");
+  closeBtn.innerHTML = "&times;";
+  closeBtn.id = "close";
+  closeBtn.onclick = closeToast;
+
+  toast.appendChild(toastBody);
+  toast.appendChild(closeBtn);
+
+  wrapper.appendChild(toast);
+
+  mainElement[0].parentElement.appendChild(wrapper);
+}, 2500);
